3761eae3e8dad6a61afcce3fd8fbca6ebaf792b8
Matías Aguirre <matiasaguirre@gmail.com>
2016-12-03 16:52:29 -0300

    Port PR #1018 from illing2005

--- social_core/backends/dropbox.py
+++ social_core/backends/dropbox.py
@@ -2,6 +2,8 @@
 Dropbox OAuth1 backend, docs at:
     http://psa.matiasaguirre.net/docs/backends/dropbox.html
 """
+import warnings
+
 from .oauth import BaseOAuth1, BaseOAuth2
 
 
@@ -19,6 +21,15 @@ class DropboxOAuth(BaseOAuth1):
         ('id', 'id'),
         ('expires', 'expires')
     ]
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            'Dropbox V1 api is deprecated and will be shute down 2017-06-28 '
+            'https://blogs.dropbox.com/developers/2016/06/api-v1-deprecated/',
+            DeprecationWarning,
+            stacklevel=2
+        )
+        super(DropboxOAuth, self).__init__(*args, **kwargs)
 
     def get_user_details(self, response):
         """Return user details from Dropbox account"""
@@ -48,6 +59,15 @@ class DropboxOAuth2(BaseOAuth2):
         ('uid', 'username'),
     ]
 
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            'Dropbox V1 api is deprecated and will be shute down 2017-06-28 '
+            'https://blogs.dropbox.com/developers/2016/06/api-v1-deprecated/',
+            DeprecationWarning,
+            stacklevel=2
+        )
+        super(DropboxOAuth2, self).__init__(*args, **kwargs)
+
     def get_user_details(self, response):
         """Return user details from Dropbox account"""
         fullname, first_name, last_name = self.get_user_names(
@@ -64,4 +84,30 @@ class DropboxOAuth2(BaseOAuth2):
         return self.get_json(
             'https://api.dropbox.com/1/account/info',
             headers={'Authorization': 'Bearer {0}'.format(access_token)}
+        )
+
+
+class DropboxOAuth2V2(BaseOAuth2):
+    name = 'dropbox-oauth2'
+    ID_KEY = 'uid'
+    AUTHORIZATION_URL = 'https://www.dropbox.com/oauth2/authorize'
+    ACCESS_TOKEN_URL = 'https://api.dropboxapi.com/oauth2/token'
+    ACCESS_TOKEN_METHOD = 'POST'
+    REDIRECT_STATE = False
+
+    def get_user_details(self, response):
+        """Return user details from Dropbox account"""
+        name = response.get('name')
+        return {'username': str(response.get('account_id')),
+                'email': response.get('email'),
+                'fullname': name.get('display_name'),
+                'first_name': name.get('given_name'),
+                'last_name': name.get('surname')}
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        return self.get_json(
+            'https://api.dropboxapi.com/2/users/get_current_account',
+            headers={'Authorization': 'Bearer {0}'.format(access_token)},
+            method='POST'
         )
