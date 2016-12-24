e2e3a25561068809421bad9360f9fd5885fcd492
Matías Aguirre <matiasaguirre@gmail.com>
2016-12-24 10:08:08 -0300

    Wrap value to ensure text type

--- social_core/utils.py
+++ social_core/utils.py
@@ -133,7 +133,7 @@ def slugify(value):
     """Converts to lowercase, removes non-word characters (alphanumerics
     and underscores) and converts spaces to hyphens. Also strips leading
     and trailing whitespace."""
-    value = unicodedata.normalize('NFKD', value) \
+    value = unicodedata.normalize('NFKD', six.text_type(value)) \
                        .encode('ascii', 'ignore') \
                        .decode('ascii')
     value = re.sub('[^\w\s-]', '', value).strip().lower()
