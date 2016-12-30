f3eacc86bf269495864ed7ae7aaa074590be5b45
Matías Aguirre <matiasaguirre@gmail.com>
2016-12-30 11:38:03 -0300

    Reorganize requirements, make OpenIdConnect optional

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ## [Unreleased](https://github.com/python-social-auth/social-core/commits/master)
 
 ### Changed
+- Reorganize requirements, make OpenIdConnect optional
 - Split OpenIdConnect from OpenId module
 
 ## [0.1.0](https://github.com/python-social-auth/social-core/releases/tag/0.1.0) - 2016-12-28
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import re
 from os.path import join, dirname, split
 from setuptools import setup
 
 
-version = __import__('social_core').__version__
+VERSION_RE = re.compile('__version__ = \'([\d\.]+)\'')
 
 LONG_DESCRIPTION = """
 Python Social Auth is an easy to setup social authentication/registration
@@ -22,18 +23,47 @@ def long_description():
     except IOError:
         return None
 
+def read_version():
+    with open('social_core/__init__.py') as file:
+        version_line = [line for line in file.readlines()
+                        if line.startswith('__version__')][0]
+        return VERSION_RE.match(version_line).groups()[0]
+
+def read_requirements(filename):
+    with open(filename, 'r') as file:
+        return [line for line in file.readlines() if not file.startswith('-')]
+
+def read_tests_requirements(filename):
+    return read_requirements('social_core/tests/{0}'.format(filename))
+
+requirements_base = read_requirements('requirements-base.txt')
+requirements_python2 = read_requirements('requirements-python2.txt')
+requirements_python3 = read_requirements('requirements-python3.txt')
+requirements_openidconnect = read_requirements('requirements-openidconnect.txt')
+
+tests_requirements_base = read_tests_requirements('requirements-base.txt')
+tests_requirements_python2 = read_tests_requirements('requirements-python2.txt')
+tests_requirements_python3 = read_tests_requirements('requirements-python3.txt')
+tests_requirements_pypy = read_tests_requirements('requirements-pypy.txt')
+
+requirements = []
+requirements.extend(requirements_base)
+
+tests_requirements = []
+tests_requirements.extend(tests_requirements_base)
+
 if os.environ.get('BUILD_VERSION') == '3' or sys.version_info[0] == 3:
-    requirements = open('requirements-python3.txt', 'r').readlines()
-    tests_requirements = open('social_core/tests/requirements-python3.txt',
-                              'r').readlines()
+    requirements.extend(requirements_python3)
+    tests_requirements.extend(tests_requirements_python3)
+elif '__pypy__' in sys.builtin_module_names:
+    tests_requirements.extend(tests_requirements_pypy)
 else:
-    requirements = open('requirements.txt', 'r').readlines()
-    tests_requirements = open('social_core/tests/requirements.txt',
-                              'r').readlines()
+    requirements.extend(requirements_python2)
+    tests_requirements.extend(tests_requirements_python2)
 
 setup(
     name='social-auth-core',
-    version=version,
+    version=read_version(),
     author='Matias Aguirre',
     author_email='matiasaguirre@gmail.com',
     description='Python social authentication made simple.',
@@ -51,6 +81,9 @@ setup(
     ],
     long_description=long_description() or LONG_DESCRIPTION,
     install_requires=requirements,
+    extras_require={
+        'openidconnect': [requirements_openidconnect]
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Topic :: Internet',
