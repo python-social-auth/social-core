5419ceba7acfcb61bf20627c97511afb599ffd28
Frank Moss <frank.moss@centurylink.com>
2017-06-20 12:01:18 -0500

    In response to feature request. Issue #96 https://github.com/python-social-auth/social-core/issues/96 Add OAuth2.0 support for Bungie.net Cleaned up a few typos Changed post authentication endpoint to an auth validating endpoint Removed all print statements

--- social_core/backends/bungie.py
+++ social_core/backends/bungie.py
@@ -48,10 +48,8 @@ class BungieOAuth2(BaseOAuth2):
 
     def auth_complete(self, *args, **kwargs):
         """Completes login process, must return user instance"""
-        print('ENTERING AUTH COMPLETE')
         self.process_error(self.data)
         state = self.validate_state()
-        # print('authcomplete', kwargs)
         response = self.request_access_token(
             self.access_token_url(),
             data=self.auth_complete_params(state),
@@ -64,7 +62,6 @@ class BungieOAuth2(BaseOAuth2):
 
     def do_auth(self, access_token, *args, **kwargs):
         """Finish the auth process once the access_token was retrieved"""
-        print('ENTERING DO AUTH')
         data = self.user_data(access_token, *args, **kwargs)
         response = kwargs.get('response') or {}
         response.update(data or {})
@@ -75,53 +72,19 @@ class BungieOAuth2(BaseOAuth2):
 
     def user_data(self, access_token, *args, **kwargs):
         """Grab user profile information from Bunige"""
-        print('ENTERING USER_DATA')
-        # print('kwargs', kwargs)
-        # print('access_token: ', access_token)
         membership_id = kwargs['response']['membership_id']
-        url = 'https://www.bungie.net/Platform/User/GetMembershipsById/' + str(membership_id) + '/254/'
+        url = 'https://www.bungie.net/Platform/User/GetBungieNetUser/'
         this_json = self.make_bungie_request(url, access_token, kwargs)
-        # print('this json', this_json)
-        username = this_json['Response']['bungieNetUser']['displayName']
-        bnId = this_json['Response']['destinyMemberships'][0]['membershipId']
-        return {'username': username, 'uid': membership_id, 'bnId': bnId}
+        username = this_json['Response']['user']['displayName']
+        return {'username': username, 'uid': membership_id}
 
     def get_user_details(self, response, *args, **kwargs):
         """Return user details from Bungie account"""
-        print('ENTERING GET USER DETAILS')
-        # Get Username from Bungie's Display Name
         username = response['username']
         uid = response['uid']
         bnId = response['bnId']
-        # print(username)
-        # Check Guardian Table for Email Address.
-        from UserInterface.models import Guardian
-        this_return = Guardian.objects.filter(membershipid=uid).values('email')
-        email = this_return[0]['email']
         return {
             'first_name': username,
             'username': username,
             'uid': uid,
-            'bnId': bnId,
-            'email': email,
         }
-
-    def auth_allowed(self, response, details):
-        """Return True if the user should be allowed to authenticate, by
-        default check if email is whitelisted (if there's a whitelist)
-        Here we check the guardian table for the presence of the membership id
-        for whitelisting."""
-        print('ENTERING BUNGIE AUTH ALLOWED')
-        # print('response', response)
-        from UserInterface.models import Guardian
-        these_membership_ids = Guardian.objects.values_list('membershipid', flat=True)
-        this_membership_id = response['membership_id']
-        # print('these_membership_ids', these_membership_ids)
-        # type(these_membership_ids)
-        # allowed = False
-        if this_membership_id in these_membership_ids:
-            allowed = True
-        else:
-            allowed = False
-        # print('ALLOWED:', allowed)
-        return allowed
