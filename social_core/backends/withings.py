5e21a1f8f1c1543faabe65fc9b7272ce53bc4e3c
Matias Aguirre <matiasaguirre@gmail.com>
2017-06-22 18:01:04 -0300

    Update withings endpoints

--- social_core/backends/withings.py
+++ social_core/backends/withings.py
@@ -3,9 +3,9 @@ from .oauth import BaseOAuth1
 
 class WithingsOAuth(BaseOAuth1):
     name = 'withings'
-    AUTHORIZATION_URL = 'https://oauth.withings.com/account/authorize'
-    REQUEST_TOKEN_URL = 'https://oauth.withings.com/account/request_token'
-    ACCESS_TOKEN_URL = 'https://oauth.withings.com/account/access_token'
+    AUTHORIZATION_URL = 'https://developer.health.nokia.com/account/authorize'
+    REQUEST_TOKEN_URL = 'https://developer.health.nokia.com/account/request_token'
+    ACCESS_TOKEN_URL = 'https://developer.health.nokia.com/account/access_token'
     ID_KEY = 'userid'
 
     def get_user_details(self, response):
