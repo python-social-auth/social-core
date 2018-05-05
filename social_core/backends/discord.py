665eb8182e57e729790e83d2bf925e67ab864e6e
Dmitry <dimasir00@yandex.ru>
2018-05-05 19:10:55 +0700

    Update discord backend
    Discord always asks permission

--- social_core/backends/discord.py
+++ social_core/backends/discord.py
@@ -10,12 +10,14 @@ class DiscordOAuth2(BaseOAuth2):
     AUTHORIZATION_URL = 'https://discordapp.com/api/oauth2/authorize'
     ACCESS_TOKEN_URL = 'https://discordapp.com/api/oauth2/token'
     ACCESS_TOKEN_METHOD = 'POST'
+    REVOKE_TOKEN_URL = 'https://discordapp.com/api/oauth2/token/revoke'
+    REVOKE_TOKEN_METHOD = 'GET'
     DEFAULT_SCOPE = ['identify']
     SCOPE_SEPARATOR = '+'
     REDIRECT_STATE = False
     EXTRA_DATA = [
         ('expires_in', 'expires'),
-        ('refresh_token', 'refresh_token', True)
+        ('refresh_token', 'refresh_token')
     ]
 
     def get_user_details(self, response):
