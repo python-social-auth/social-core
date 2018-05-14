a31358e118b839059f8756da841b4ac7a41984bd
Callum <callum@pixelpin.co.uk>
2018-05-14 17:23:03 +0100

    ISSUE-231 Updated pixelpin backend to use OpenID Connect and use new endpoints

--- social_core/backends/pixelpin.py
+++ social_core/backends/pixelpin.py
@@ -1,33 +1,35 @@
-from .oauth import BaseOAuth2
+from .open_id_connect import OpenIdConnectAuth
+import json
 
 
-class PixelPinOAuth2(BaseOAuth2):
-    """PixelPin OAuth authentication backend"""
-    name = 'pixelpin-oauth2'
-    ID_KEY = 'id'
-    AUTHORIZATION_URL = 'https://login.pixelpin.co.uk/OAuth2/Flogin.aspx'
-    ACCESS_TOKEN_URL = 'https://ws3.pixelpin.co.uk/index.php/api/token'
+class PixelPinOpenIDConnect(OpenIdConnectAuth):
+    """PixelPin OpenID Connect authentication backend"""
+    name = 'pixelpin-openidconnect'
+    ID_KEY = 'sub'
+    AUTHORIZATION_URL = 'https://login.pixelpin.io/connect/authorize'
+    ACCESS_TOKEN_URL = 'https://login.pixelpin.io/connect/token'
+    OIDC_ENDPOINT = 'https://login.pixelpin.io'
+    JWKS_URI = 'https://login.pixelpin.io/.well-known/jwks'
     ACCESS_TOKEN_METHOD = 'POST'
     REQUIRES_EMAIL_VALIDATION = False
-    EXTRA_DATA = [
-        ('id', 'id'),
-    ]
 
     def get_user_details(self, response):
         """Return user details from PixelPin account"""
-        fullname, first_name, last_name = self.get_user_names(
-            first_name=response.get('firstName'),
-            last_name=response.get('lastName')
-        )
-        return {'username': response.get('firstName'),
-                'email': response.get('email') or '',
-                'fullname': fullname,
+
+        first_name = response.get('given_name')
+        last_name = response.get('family_name')
+        sub = response.get('sub')
+
+        username = first_name + last_name + sub
+
+        return {'username': username,
+                'email': response.get('email'),
+                'fullname': response.get('given_name') + ' ' + response.get('family_name'),
                 'first_name': first_name,
                 'last_name': last_name}
-
+    
+	
     def user_data(self, access_token, *args, **kwargs):
-        """Loads user data from service"""
-        return self.get_json(
-            'https://ws3.pixelpin.co.uk/index.php/api/userdata',
-            params={'access_token': access_token}
-        )
+        return self.get_json('https://login.pixelpin.io/connect/userinfo', headers={
+            'Authorization': 'Bearer {0}'.format(access_token)
+        })
