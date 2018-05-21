1d809941ab8a99af9e1bdf12bae548202c94eaa2
Matias Aguirre <matiasaguirre@gmail.com>
2018-05-21 12:56:08 -0300

    Correct tests cases on slack for teams

--- social_core/tests/backends/test_slack.py
+++ social_core/tests/backends/test_slack.py
@@ -31,6 +31,23 @@ class SlackOAuth2Test(OAuth2Test):
         self.do_partial_pipeline()
 
 
+class SlackOAuth2TestTeamName(SlackOAuth2Test):
+    expected_username = 'foobar@Square'
+    user_data_body = json.dumps({
+        'ok': True,
+        'user': {
+            'email': 'foobar@example.com',
+            'name': 'Foo Bar',
+            'id': u'123456'
+        },
+        'team': {
+            'id': u'456789',
+            'name': u'Square',
+        },
+        'scope': u'identity.basic,identity.email,identity.team'
+    })
+
+
 class SlackOAuth2TestUnicodeTeamName(SlackOAuth2Test):
     user_data_body = json.dumps({
         'ok': True,
@@ -46,3 +63,14 @@ class SlackOAuth2TestUnicodeTeamName(SlackOAuth2Test):
         'scope': u'identity.basic,identity.email,identity.team'
     })
 
+    def test_login(self):
+        self.strategy.set_settings({
+            'SOCIAL_AUTH_SLACK_USERNAME_WITH_TEAM': False
+        })
+        self.do_login()
+
+    def test_partial_pipeline(self):
+        self.strategy.set_settings({
+            'SOCIAL_AUTH_SLACK_USERNAME_WITH_TEAM': False
+        })
+        self.do_partial_pipeline()
