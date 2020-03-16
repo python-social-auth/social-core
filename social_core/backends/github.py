f3962e63cdf3e1b9853f180809efdd9d2be7d2d7
Matias Aguirre <matiasaguirre@gmail.com>
2020-03-16 12:35:42 -0300

    Style and changelog entry

--- social_core/backends/github.py
+++ social_core/backends/github.py
@@ -128,21 +128,21 @@ class GithubAppAuth(GithubOAuth2):
 
     def validate_state(self):
         """
-            Scenario 1: user clicks an icon/button on your website and
-                initiates social login. This works exacltly like standard
-                OAuth and we have `state` and `redirect_uri`.
+        Scenario 1: user clicks an icon/button on your website and initiates
+                    social login. This works exacltly like standard OAuth and we
+                    have `state` and `redirect_uri`.
 
-            Scenario 2: user starts from http://github.com/apps/your-app
-                and clicks 'Install & Authorize' button! They still get
-                a temporary `code` (used to fetch `access_token`) but
-                there's no `state` or `redirect_uri` here.
+        Scenario 2: user starts from http://github.com/apps/your-app and clicks
+                    'Install & Authorize' button! They still get a temporary
+                    `code` (used to fetch `access_token`) but there's no `state`
+                    or `redirect_uri` here.
 
-            Note: Scenario 2 only happens when your GitHub App is configured
-                with `Request user authorization (OAuth) during installation`
-                turned on! This causes GitHub to redirect the person back to
-                `/complete/github/`. If the above setting is turned off then
-                GitHub will redirect to another URL called Setup URL and the
-                person may need to login first before they can continue!
+        Note: Scenario 2 only happens when your GitHub App is configured
+              with `Request user authorization (OAuth) during installation`
+              turned on! This causes GitHub to redirect the person back to
+              `/complete/github/`. If the above setting is turned off then
+              GitHub will redirect to another URL called Setup URL and the
+              person may need to login first before they can continue!
         """
         if self.data.get('installation_id') and self.data.get('setup_action'):
             return None
