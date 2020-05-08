3b5708b752951b4709f7df25b01400632607120f
Nana Adjedu <nanakappanana@gmail.com>
2020-05-08 19:55:52 -0400

    self.strategy.authenticate expects backend argument

--- social_core/backends/instagram.py
+++ social_core/backends/instagram.py
@@ -53,6 +53,17 @@ class InstagramOAuth2(BaseOAuth2):
                                      data.get('error_code'))
 
     @handle_http_errors
+    def do_auth(self, access_token, *args, **kwargs):
+        """Finish the auth process once the access_token was retrieved"""
+        data = self.user_data(access_token, *args, **kwargs)
+        response = kwargs.get('response') or {}
+        response.update(data or {})
+        if 'access_token' not in response:
+            response['access_token'] = access_token
+        kwargs.update({'response': response})
+        return self.strategy.authenticate(backend=self, *args, **kwargs)
+
+    @handle_http_errors
     def auth_complete(self, *args, **kwargs):
         """Completes login process, must return user instance"""
         self.process_error(self.data)
