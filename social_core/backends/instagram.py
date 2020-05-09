88bef53734e80e826f2af1506923399badaa53c2
Nana Adjedu <nanakappanana@gmail.com>
2020-05-08 22:30:34 -0400

    fixed self.strategy.authenticate self args

--- social_core/backends/instagram.py
+++ social_core/backends/instagram.py
@@ -49,17 +49,6 @@ class InstagramOAuth2(BaseOAuth2):
                                      data.get('error_code'))
 
     @handle_http_errors
-    def do_auth(self, access_token, *args, **kwargs):
-        """Finish the auth process once the access_token was retrieved"""
-        data = self.user_data(access_token, *args, **kwargs)
-        response = kwargs.get('response') or {}
-        response.update(data or {})
-        if 'access_token' not in response:
-            response['access_token'] = access_token
-        kwargs.update({'response': data})
-        return self.strategy.authenticate(self, *args, **kwargs)
-
-    @handle_http_errors
     def auth_complete(self, *args, **kwargs):
         """Completes login process, must return user instance"""
         self.process_error(self.data)
@@ -86,7 +75,7 @@ class InstagramOAuth2(BaseOAuth2):
         except ValueError:
             response = parse_qs(response.text)
         access_token = response['access_token']
-        return self.do_auth(access_token, response, *args, **kwargs)
+        return self.do_auth(access_token, response=response, *args, **kwargs)
 
 
     def refresh_token_params(self, token, *args, **kwargs):
