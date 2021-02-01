f806160479440d30fe7bede08b11fc3f22dc26b9
Michael Roberts <michael@hiyield.co.uk>
2021-02-01 18:02:22 +0000

    Changes to the user_data_body mock API body and the user_data_url.

--- social_core/tests/backends/test_orcid.py
+++ social_core/tests/backends/test_orcid.py
@@ -6,16 +6,17 @@ from social_core.backends.orcid import ORCIDOAuth2
 
 class ORCIDOAuth2Test(OAuth2Test):
     backend_path = 'social_core.backends.orcid.ORCIDOAuth2'
-    user_data_url = 'https://login.orbi.kr/oauth/user/get'
-    expected_username = 'foobar'
+    user_data_url = 'https://orcid.org/oauth/userinfo'
+    expected_username = '0000-0002-2601-8132'
     access_token_body = json.dumps({
         'access_token': 'foobar',
+        'token_type': 'bearer'
     })
     user_data_body = json.dumps({
-        'sub': '0000-0002-2601-8132',
-        'name': 'Credit Name',
-        'family_name': 'Jones',
-        'given_name': 'Tom'
+        "sub":"0000-0002-2601-8132",
+        "name":"Credit Name",
+        "family_name":"Jones",
+        "given_name":"Tom"
     })
 
     def test_login(self):
