7979be81b62ec4761252637b692ff61ece8d3909
Jeppe Fihl-Pearson <jeppe@memrise.com>
2021-05-19 10:47:35 +0100

    There's no access token to call APIs to get user data, so make it a no-op

--- social_core/backends/facebook.py
+++ social_core/backends/facebook.py
@@ -273,3 +273,7 @@ class FacebookLimitedLogin(OpenIdConnectAuth):
             "email": response.get("email"),
             "picture": response.get("picture"),
         }
+
+    def user_data(self, access_token, *args, **kwargs):
+        # We don't have an access token to call any API for the user details.
+        return None
