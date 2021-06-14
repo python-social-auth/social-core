e288902653337464bd0faa07b30cb55762535690
Chenxiong Qi <qcxhome@gmail.com>
2021-06-14 12:33:35 +0800

    Use utcnow for JANRAIN_NONCE
    The OpenIdStore.useNonce uses non-local time.
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- social_core/tests/backends/test_livejournal.py
+++ social_core/tests/backends/test_livejournal.py
@@ -8,7 +8,7 @@ from ...exceptions import AuthMissingParameter
 from .open_id import OpenIdTest
 
 
-JANRAIN_NONCE = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+JANRAIN_NONCE = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
 class LiveJournalOpenIdTest(OpenIdTest):
