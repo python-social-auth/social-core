17476e32ec16d3d0a4c261947a1dd814f3432bd5
Michal Čihař <michal@cihar.com>
2021-10-13 10:27:16 +0200

    facebook: Update API version to current one

--- social_core/backends/facebook.py
+++ social_core/backends/facebook.py
@@ -14,7 +14,7 @@ from ..exceptions import AuthException, AuthCanceled, AuthUnknownError, \
                          AuthMissingParameter
 
 
-API_VERSION = 8.0
+API_VERSION = 14.0
 
 
 class FacebookOAuth2(BaseOAuth2):
