40141c7ab62fceb7823a382c7f2b821624b881ba
Brian Candler <b.candler@pobox.com>
2022-03-02 09:44:02 +0000

    Fix for Keycloak backend
    Without this fix, PSA appends ?redirect_state=XXXX to the
    redirect_uri parameter value passed to the auth endpoint, and
    Keycloak rejects this with "Invalid parameter: redirect_uri"
    
    Tested with:
    - keycloak 17.0.0
    - social-auth-core==4.2.0 plus this patch

--- social_core/backends/keycloak.py
+++ social_core/backends/keycloak.py
@@ -88,6 +88,7 @@ class KeycloakOAuth2(BaseOAuth2):  # pylint: disable=abstract-method
     name = 'keycloak'
     ID_KEY = 'username'
     ACCESS_TOKEN_METHOD = 'POST'
+    REDIRECT_STATE = False
 
     def authorization_url(self):
         return self.setting('AUTHORIZATION_URL')
