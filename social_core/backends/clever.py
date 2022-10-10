740227835f4e6c1afdd8c555a781a632e6ed8195
Jaimin Patel <jpatel@tuvalabs.com>
2022-10-10 13:40:21 -0400

    fixed flask8 errors #716

--- social_core/backends/clever.py
+++ social_core/backends/clever.py
@@ -17,23 +17,23 @@ class CleverOAuth2(BaseOAuth2):
 
     def get_user_id(self, details, response):
         """Return user unique id provided by service"""
-        return response.get("data", {}).get("id")
+        return response.get('data', {}).get('id')
 
     def get_user_type(self, data):
-        return list(data.get("data", {}).get("roles", {}).keys())[0]
+        return list(data.get('data', {}).get('roles', {}).keys())[0]
 
     def get_user_details(self, response):
         """Return user details from Classlink account"""
         fullname, first_name, last_name = self.get_user_names(
-            first_name=response.get("data", {}).get("name", {}).get("first", None),
-            last_name=response.get("data", {}).get("name", {}).get("last", None)
+            first_name=response.get('data', {}).get('name', {}).get('first', None),
+            last_name=response.get('data', {}).get('name', {}).get('last', None)
         )
-        email = response.get("data", {}).get("email")
-        username = response.get("data", {}).get(
-            "roles", {}).get(
+        email = response.get('data', {}).get('email')
+        username = response.get('data', {}).get(
+            'roles', {}).get(
                 self.get_user_type(response), {}).get(
-                    "credentials", {}).get(
-                        "district_username", email.split('@', 1)[0])
+                    'credentials', {}).get(
+                        'district_username', email.split('@', 1)[0])
         return {
             'username': username,
             'email': email,
@@ -50,7 +50,7 @@ class CleverOAuth2(BaseOAuth2):
         auth_header = {'Authorization': 'Bearer %s' % token}
         try:
             response = self.get_json(identity_url, headers=auth_header)
-            user_id = response.get("data", {}).get("id")
+            user_id = response.get('data', {}).get('id')
             user_details_url = 'https://api.clever.com/v3.0/users/%s' % user_id
             return self.get_json(user_details_url, headers=auth_header)
         except ValueError:
