7afcb668f89c9cbeb6d860134c7921819c66ba39
LA <675385864@qq.com>
2022-12-11 19:45:45 +0800

    Support Casdoor SSO autentication in social-app-django

---
+++ social_core/backends/casdoor.py
@@ -0,0 +1,214 @@
+import requests
+import jwt
+import json
+# from .user import User
+from typing import List
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from django.apps import AppConfig
+from django_user import settings
+
+
+class User:
+    def __init__(self):
+        self.address = ["string"]
+        self.affiliation = "string"
+        self.avatar = "string"
+        self.createdTime = "string"
+        self.dingtalk = "string"
+        self.displayName = "string"
+        self.email = "string"
+        self.facebook = "string"
+        self.gitee = "string"
+        self.github = "string"
+        self.google = "string"
+        self.hash = "string"
+        self.id = "string"
+        self.isAdmin = True
+        self.isForbidden = True
+        self.isGlobalAdmin = True
+        self.language = "string"
+        self.name = "string"
+        self.owner = "string"
+        self.password = "string"
+        self.phone = "string"
+        self.preHash = "string"
+        self.qq = "string"
+        self.score = 0
+        self.signupApplication = "string"
+        self.tag = "string"
+        self.type = "string"
+        self.updatedTime = "string"
+        self.wechat = "string"
+        self.weibo = "string"
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+class CasdoorSDK:
+    name = "casdoor"
+
+    def __init__(
+        self,
+        endpoint: str,
+        client_id: str,
+        client_secret: str,
+        certificate: str,
+        org_name: str,
+        application_name: str,
+        front_endpoint: str = None
+    ):
+        self.endpoint = endpoint
+        if front_endpoint:
+            self.front_endpoint = front_endpoint
+        else:
+            self.front_endpoint = endpoint.replace(":8000", ":7001")
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.certificate = certificate
+        self.org_name = org_name
+        self.application_name = application_name
+        self.grant_type = "authorization_code"
+
+        self.algorithms = ["RS256"]
+
+    @property
+    def certification(self) -> bytes:
+        if type(self.certificate) is not str:
+            raise TypeError('certificate field must be str type')
+        return self.certificate.encode('utf-8')
+
+    def get_auth_link(self, redirect_uri: str, response_type: str = "code", scope: str = "read"):
+        url = self.front_endpoint + "/login/oauth/authorize"
+        params = {
+            "client_id": self.client_id,
+            "response_type": response_type,
+            "redirect_uri": redirect_uri,
+            "scope": scope,
+            "state": self.application_name,
+        }
+        r = requests.request("", url, params=params)
+        return r.url
+
+    def get_oauth_token(self, code: str) -> str:
+        """
+        Request the Casdoor server to get access_token.
+        :param code: the code that sent from Casdoor using redirect url back to your server.
+        :return: access_token
+        """
+        url = self.endpoint + "/api/login/oauth/access_token"
+        params = {
+            "grant_type": self.grant_type,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "code": code,
+        }
+        r = requests.post(url, params)
+        access_token = r.json().get("access_token")
+
+        return access_token
+
+    def parse_jwt_token(self, token: str) -> dict:
+        """
+        Converts the returned access_token to real data using jwt (JSON Web Token) algorithms.
+        :param token: access_token
+        :return: the data in dict format
+        """
+        certificate = x509.load_pem_x509_certificate(self.certification, default_backend())
+
+        return_json = jwt.decode(
+            token,
+            certificate.public_key(),
+            algorithms=self.algorithms,
+            audience=self.client_id,
+        )
+        return return_json
+
+    def get_users(self) -> List[dict]:
+        """
+        Get the users from Casdoor.
+        :return: a list of dicts containing user info
+        """
+        url = self.endpoint + "/api/get-users"
+        params = {
+            "owner": self.org_name,
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        r = requests.get(url, params)
+        users = r.json()
+        return users
+
+    def get_user(self, user_id: str) -> dict:
+        """
+        Get the user from Casdoor providing the user_id.
+        :param user_id: the id of the user
+        :return: a dict that contains user's info
+        """
+        url = self.endpoint + "/api/get-user"
+        params = {
+            "id": f"{self.org_name}/{user_id}",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        r = requests.get(url, params)
+        user = r.json()
+        return user
+
+    def modify_user(self, method: str, user: User) -> dict:
+        url = self.endpoint + f"/api/{method}"
+        user.owner = self.org_name
+        params = {
+            "id": f"{user.owner}/{user.name}",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        user_info = json.dumps(user.to_dict())
+        r = requests.post(url, params=params, data=user_info)
+        response = r.json()
+        return response
+
+    def add_user(self, user: User) -> dict:
+        response = self.modify_user("add-user", user)
+        return response
+
+    def update_user(self, user: User) -> dict:
+        response = self.modify_user("update-user", user)
+        return response
+
+    def delete_user(self, user: User) -> dict:
+        response = self.modify_user("delete-user", user)
+        return response
+
+# class CasdoorAuth(AppConfig):
+#     default_auto_field = "django.db.models.BigAutoField"
+#     name = "social_core"
+#
+#     conf = settings.CASDOOR_CONFIG
+#
+#     sdk = CasdoorSDK(conf.get('endpoint'),
+#                      conf.get('client_id'),
+#                      conf.get('client_secret'),
+#                      conf.get('certificate'),
+#                      conf.get('org_name'),
+#                      conf.get('application_name'),
+#                      conf.get('endpoint'))
+#
+#     def get_users(self):
+#         return self.sdk.get_users()
+#
+#     def get_user(self, request):
+#         return self.sdk.get_user(request.GET.get('name'))
+#
+#     def add_user(self, request):
+#         user = request.GET.get("name")
+#         return self.sdk.add_user(user)
+#
+#     def update_user(self, request):
+#         return self.sdk.add_user(request.GET.get('user'))
+#
+#     def delete_user(self, request):
+#         return self.sdk.delete_user(request.GET.get('name'))
---
+++ social_core/tests/backends/test_casdoor.py
@@ -0,0 +1,109 @@
+from ...backends.casdoor import CasdoorSDK, User
+from unittest import TestCase
+from django_user import settings
+from django.contrib.auth import authenticate, login
+# from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
+class TestCasdoorAuth(TestCase):
+    """
+    You should replace the code content below and
+    the get_sdk() method's content with your own Casdoor
+    instance and such if you need to. And running these tests successfully
+    proves that your connection to Casdoor is good-and-working!
+    """
+
+    backend_path = "social_core.backends.casdoor"
+
+    # server returned authorization code
+    code = "f97ae47cf361047eb7a8"
+
+
+    @staticmethod
+    def get_sdk():
+        conf = settings.CASDOOR_CONFIG
+
+        certificate = '''-----BEGIN CERTIFICATE-----
+        MIIE+TCCAuGgAwIBAgIDAeJAMA0GCSqGSIb3DQEBCwUAMDYxHTAbBgNVBAoTFENh
+        c2Rvb3IgT3JnYW5pemF0aW9uMRUwEwYDVQQDEwxDYXNkb29yIENlcnQwHhcNMjEx
+        MDE1MDgxMTUyWhcNNDExMDE1MDgxMTUyWjA2MR0wGwYDVQQKExRDYXNkb29yIE9y
+        Z2FuaXphdGlvbjEVMBMGA1UEAxMMQ2FzZG9vciBDZXJ0MIICIjANBgkqhkiG9w0B
+        AQEFAAOCAg8AMIICCgKCAgEAsInpb5E1/ym0f1RfSDSSE8IR7y+lw+RJjI74e5ej
+        rq4b8zMYk7HeHCyZr/hmNEwEVXnhXu1P0mBeQ5ypp/QGo8vgEmjAETNmzkI1NjOQ
+        CjCYwUrasO/f/MnI1C0j13vx6mV1kHZjSrKsMhYY1vaxTEP3+VB8Hjg3MHFWrb07
+        uvFMCJe5W8+0rKErZCKTR8+9VB3janeBz//zQePFVh79bFZate/hLirPK0Go9P1g
+        OvwIoC1A3sarHTP4Qm/LQRt0rHqZFybdySpyWAQvhNaDFE7mTstRSBb/wUjNCUBD
+        PTSLVjC04WllSf6Nkfx0Z7KvmbPstSj+btvcqsvRAGtvdsB9h62Kptjs1Yn7GAuo
+        I3qt/4zoKbiURYxkQJXIvwCQsEftUuk5ew5zuPSlDRLoLByQTLbx0JqLAFNfW3g/
+        pzSDjgd/60d6HTmvbZni4SmjdyFhXCDb1Kn7N+xTojnfaNkwep2REV+RMc0fx4Gu
+        hRsnLsmkmUDeyIZ9aBL9oj11YEQfM2JZEq+RVtUx+wB4y8K/tD1bcY+IfnG5rBpw
+        IDpS262boq4SRSvb3Z7bB0w4ZxvOfJ/1VLoRftjPbLIf0bhfr/AeZMHpIKOXvfz4
+        yE+hqzi68wdF0VR9xYc/RbSAf7323OsjYnjjEgInUtRohnRgCpjIk/Mt2Kt84Kb0
+        wn8CAwEAAaMQMA4wDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQsFAAOCAgEAn2lf
+        DKkLX+F1vKRO/5gJ+Plr8P5NKuQkmwH97b8CS2gS1phDyNgIc4/LSdzuf4Awe6ve
+        C06lVdWSIis8UPUPdjmT2uMPSNjwLxG3QsrimMURNwFlLTfRem/heJe0Zgur9J1M
+        8haawdSdJjH2RgmFoDeE2r8NVRfhbR8KnCO1ddTJKuS1N0/irHz21W4jt4rxzCvl
+        2nR42Fybap3O/g2JXMhNNROwZmNjgpsF7XVENCSuFO1jTywLaqjuXCg54IL7XVLG
+        omKNNNcc8h1FCeKj/nnbGMhodnFWKDTsJcbNmcOPNHo6ixzqMy/Hqc+mWYv7maAG
+        Jtevs3qgMZ8F9Qzr3HpUc6R3ZYYWDY/xxPisuKftOPZgtH979XC4mdf0WPnOBLqL
+        2DJ1zaBmjiGJolvb7XNVKcUfDXYw85ZTZQ5b9clI4e+6bmyWqQItlwt+Ati/uFEV
+        XzCj70B4lALX6xau1kLEpV9O1GERizYRz5P9NJNA7KoO5AVMp9w0DQTkt+LbXnZE
+        HHnWKy8xHQKZF9sR7YBPGLs/Ac6tviv5Ua15OgJ/8dLRZ/veyFfGo2yZsI+hKVU5
+        nCCJHBcAyFnm1hdvdwEdH33jDBjNB6ciotJZrf/3VYaIWSalADosHAgMWfXuWP+h
+        8XKXmzlxuHbTMQYtZPDgspS5aK+S4Q9wb8RRAYo=
+        -----END CERTIFICATE-----
+        '''
+
+        sdk = CasdoorSDK(
+            endpoint="http://localhost:8000",
+            client_id="fdb3972770f53a59b29a",
+            client_secret="ab45c886ca7863c7cdccc7ace2d05a9ec08f1819",
+            certificate=certificate,
+            org_name="built-in",
+            application_name= "app-built-in"
+        )
+
+        return sdk
+
+    # def test_get_oauth_token(self):
+    #     sdk = self.get_sdk()
+    #     access_token = sdk.get_oauth_token(self.code)
+    #     self.assertIsInstance(access_token, str)
+    #
+    # def test_parse_jwt_token(self):
+    #     sdk = self.get_sdk()
+    #     access_token = sdk.get_oauth_token(self.code)
+    #     decoded_msg = sdk.parse_jwt_token(access_token)
+    #     self.assertIsInstance(decoded_msg, dict)
+
+    def test_get_users(self):
+        sdk = self.get_sdk()
+        users = sdk.get_users()
+        self.assertIsInstance(users, list)
+
+    def test_get_user(self):
+        sdk = self.get_sdk()
+        user = sdk.get_user("admin")
+        self.assertIsInstance(user, dict)
+
+    def test_modify_user(self):
+        sdk = self.get_sdk()
+        user = User()
+        user.name = "test_ffyuanda"
+        sdk.delete_user(user)
+
+        response = sdk.add_user(user)
+        self.assertEqual(response["data"], "Affected")
+
+        response = sdk.delete_user(user)
+        self.assertEqual(response["data"], "Affected")
+
+        response = sdk.add_user(user)
+        self.assertEqual(response["data"], "Affected")
+
+        user.phone = "phone"
+        response = sdk.update_user(user)
+        self.assertEqual(response["data"], "Affected")
+
+        self.assertIn("status", response)
+        self.assertIsInstance(response, dict)
