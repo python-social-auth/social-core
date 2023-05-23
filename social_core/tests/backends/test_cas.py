f9711a81f5085fc00f5cdad0c2e7b1a204b57f63
Simon Lyngshede <slyngshede@wikimedia.org>
2023-05-23 10:55:36 +0200

    CAS: Support group whitelisting
    Allow the user to configure a group whitelist.
    Any user not a member of the one or more of the
    whitelisted groups will not allowed to
    authenticate, if the social_auth.auth_allowed
    is enabled.

--- social_core/tests/backends/test_cas.py
+++ social_core/tests/backends/test_cas.py
@@ -47,6 +47,8 @@ class CASOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
         settings.update(
             {
                 f"SOCIAL_AUTH_{self.name}_OIDC_ENDPOINT": f"{ROOT_URL}oidc",
+                f"SOCIAL_AUTH_{self.name}_ALLOW_GROUPS": ["users",],
+                f"SOCIAL_AUTH_{self.name}_WHITELISTED_DOMAINS": ["example.net",]
             }
         )
         return settings
