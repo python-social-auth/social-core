6a2734e5e9c5821b126869e6aa776ba8237a9ea7
Michal Čihař <michal@cihar.com>
2023-09-12 07:54:27 +0200

    backends(ping): coding style fixes

--- social_core/backends/ping.py
+++ social_core/backends/ping.py
@@ -2,31 +2,29 @@
 Ping Auth OpenID Connect backend
 """
 
-from os import stat
-from social_core.backends.open_id_connect import OpenIdConnectAuth
-
 from jose import jwk, jwt
-from jose.jwt import JWTError, JWTClaimsError, ExpiredSignatureError
+from jose.jwt import ExpiredSignatureError, JWTClaimsError, JWTError
 from jose.utils import base64url_decode
 
+from social_core.backends.open_id_connect import OpenIdConnectAuth
 from social_core.exceptions import AuthTokenError
 
 
 class PingOpenIdConnect(OpenIdConnectAuth):
-    name = 'ping'
+    name = "ping"
     # OIDC_ENDPOINT has the form 'https://auth.pingone.com/<APP ID>/as'
-    OIDC_ENDPOINT = ''
+    OIDC_ENDPOINT = ""
     REDIRECT_STATE = False
-    ACCESS_TOKEN_METHOD = 'POST'
-    RESPONSE_TYPE = 'code'
-    USERNAME_KEY = 'preferred_username'
+    ACCESS_TOKEN_METHOD = "POST"
+    RESPONSE_TYPE = "code"
+    USERNAME_KEY = "preferred_username"
 
     def find_valid_key(self, id_token):
         for key in self.get_jwks_keys():
-            rsakey = jwk.construct(key, algorithm='RS256')
-            message, encoded_sig = id_token.rsplit('.', 1)
-            decoded_sig = base64url_decode(encoded_sig.encode('utf-8'))
-            if rsakey.verify(message.encode('utf-8'), decoded_sig):
+            rsakey = jwk.construct(key, algorithm="RS256")
+            message, encoded_sig = id_token.rsplit(".", 1)
+            decoded_sig = base64url_decode(encoded_sig.encode("utf-8"))
+            if rsakey.verify(message.encode("utf-8"), decoded_sig):
                 return key
 
     def validate_and_return_id_token(self, id_token, access_token):
@@ -39,14 +37,14 @@ class PingOpenIdConnect(OpenIdConnectAuth):
         key = self.find_valid_key(id_token)
 
         if not key:
-            raise AuthTokenError(self, 'Signature verification failed')
+            raise AuthTokenError(self, "Signature verification failed")
 
-        rsakey = jwk.construct(key, algorithm='RS256')
+        rsakey = jwk.construct(key, algorithm="RS256")
 
         try:
             claims = jwt.decode(
                 id_token,
-                rsakey.to_pem().decode('utf-8'),
+                rsakey.to_pem().decode("utf-8"),
                 algorithms=self.JWT_ALGORITHMS,
                 audience=client_id,
                 issuer=self.id_token_issuer(),
@@ -54,23 +52,25 @@ class PingOpenIdConnect(OpenIdConnectAuth):
                 options=self.JWT_DECODE_OPTIONS,
             )
         except ExpiredSignatureError:
-            raise AuthTokenError(self, 'Signature has expired')
+            raise AuthTokenError(self, "Signature has expired")
         except JWTClaimsError as error:
             raise AuthTokenError(self, str(error))
         except JWTError:
-            raise AuthTokenError(self, 'Invalid signature')
+            raise AuthTokenError(self, "Invalid signature")
 
         self.validate_claims(claims)
 
         return claims
 
     def get_user_details(self, response):
-        username_key = self.setting('USERNAME_KEY', default=self.USERNAME_KEY)
+        username_key = self.setting("USERNAME_KEY", default=self.USERNAME_KEY)
         fullname, first_name, last_name = self.get_user_names(
-            first_name=response.get('given_name'),
-            last_name=response.get('family_name'))
-        return {'username': response.get(username_key),
-                'email': response.get('email'),
-                'fullname': fullname,
-                'first_name': first_name,
-                'last_name': last_name}
+            first_name=response.get("given_name"), last_name=response.get("family_name")
+        )
+        return {
+            "username": response.get(username_key),
+            "email": response.get("email"),
+            "fullname": fullname,
+            "first_name": first_name,
+            "last_name": last_name,
+        }
