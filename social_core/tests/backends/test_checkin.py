c952fbd067ac26b871f2e17c687c3a9933c969ab
Enol Fernandez <enol.fernandez@egi.eu>
2023-10-04 16:21:02 +0100

    Fix json string in test

--- social_core/tests/backends/test_checkin.py
+++ social_core/tests/backends/test_checkin.py
@@ -13,8 +13,8 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
        "introspection_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/token/introspect",
        "userinfo_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/userinfo",
        "end_session_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/logout",
-       "frontchannel_logout_session_supported": True,
-       "frontchannel_logout_supported": True,
+       "frontchannel_logout_session_supported": true,
+       "frontchannel_logout_supported": true,
        "jwks_uri": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/certs",
        "check_session_iframe": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/login-status-iframe.html",
        "grant_types_supported": [
@@ -25,7 +25,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "client_credentials",
            "urn:ietf:params:oauth:grant-type:device_code",
            "urn:openid:params:grant-type:ciba",
-           "urn:ietf:params:oauth:grant-type:token-exchange",
+           "urn:ietf:params:oauth:grant-type:token-exchange"
        ],
        "acr_values_supported": ["0", "1"],
        "response_types_supported": [
@@ -36,7 +36,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "id_token token",
            "code id_token",
            "code token",
-           "code id_token token",
+           "code id_token token"
        ],
        "subject_types_supported": ["public", "pairwise"],
        "id_token_signing_alg_values_supported": [
@@ -51,12 +51,12 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "ES512",
            "PS256",
            "PS512",
-           "RS512",
+           "RS512"
        ],
        "id_token_encryption_alg_values_supported": [
            "RSA-OAEP",
            "RSA-OAEP-256",
-           "RSA1_5",
+           "RSA1_5"
        ],
        "id_token_encryption_enc_values_supported": [
            "A256GCM",
@@ -64,7 +64,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "A128GCM",
            "A128CBC-HS256",
            "A192CBC-HS384",
-           "A256CBC-HS512",
+           "A256CBC-HS512"
        ],
        "userinfo_signing_alg_values_supported": [
            "PS384",
@@ -79,12 +79,12 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "PS256",
            "PS512",
            "RS512",
-           "none",
+           "none"
        ],
        "userinfo_encryption_alg_values_supported": [
            "RSA-OAEP",
            "RSA-OAEP-256",
-           "RSA1_5",
+           "RSA1_5"
        ],
        "userinfo_encryption_enc_values_supported": [
            "A256GCM",
@@ -92,7 +92,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "A128GCM",
            "A128CBC-HS256",
            "A192CBC-HS384",
-           "A256CBC-HS512",
+           "A256CBC-HS512"
        ],
        "request_object_signing_alg_values_supported": [
            "PS384",
@@ -107,12 +107,12 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "PS256",
            "PS512",
            "RS512",
-           "none",
+           "none"
        ],
        "request_object_encryption_alg_values_supported": [
            "RSA-OAEP",
            "RSA-OAEP-256",
-           "RSA1_5",
+           "RSA1_5"
        ],
        "request_object_encryption_enc_values_supported": [
            "A256GCM",
@@ -120,7 +120,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "A128GCM",
            "A128CBC-HS256",
            "A192CBC-HS384",
-           "A256CBC-HS512",
+           "A256CBC-HS512"
        ],
        "response_modes_supported": [
            "query",
@@ -129,7 +129,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "query.jwt",
            "fragment.jwt",
            "form_post.jwt",
-           "jwt",
+           "jwt"
        ],
        "registration_endpoint": "https://aai.egi.eu/auth/realms/egi/clients-registrations/openid-connect",
        "token_endpoint_auth_methods_supported": [
@@ -137,7 +137,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "client_secret_basic",
            "client_secret_post",
            "tls_client_auth",
-           "client_secret_jwt",
+           "client_secret_jwt"
        ],
        "token_endpoint_auth_signing_alg_values_supported": [
            "PS384",
@@ -151,14 +151,14 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "ES512",
            "PS256",
            "PS512",
-           "RS512",
+           "RS512"
        ],
        "introspection_endpoint_auth_methods_supported": [
            "private_key_jwt",
            "client_secret_basic",
            "client_secret_post",
            "tls_client_auth",
-           "client_secret_jwt",
+           "client_secret_jwt"
        ],
        "introspection_endpoint_auth_signing_alg_values_supported": [
            "PS384",
@@ -172,7 +172,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "ES512",
            "PS256",
            "PS512",
-           "RS512",
+           "RS512"
        ],
        "authorization_signing_alg_values_supported": [
            "PS384",
@@ -186,12 +186,12 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "ES512",
            "PS256",
            "PS512",
-           "RS512",
+           "RS512"
        ],
        "authorization_encryption_alg_values_supported": [
            "RSA-OAEP",
            "RSA-OAEP-256",
-           "RSA1_5",
+           "RSA1_5"
        ],
        "authorization_encryption_enc_values_supported": [
            "A256GCM",
@@ -199,7 +199,7 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "A128GCM",
            "A128CBC-HS256",
            "A192CBC-HS384",
-           "A256CBC-HS512",
+           "A256CBC-HS512"
        ],
        "claims_supported": [
            "acr",
@@ -219,10 +219,10 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "sub",
            "voperson_external_affiliation",
            "voperson_id",
-           "voperson_verified_email",
+           "voperson_verified_email"
        ],
        "claim_types_supported": ["normal"],
-       "claims_parameter_supported": True,
+       "claims_parameter_supported": true,
        "scopes_supported": [
            "openid",
            "voperson_external_affiliation",
@@ -236,20 +236,20 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "profile",
            "offline_access",
            "eduperson_unique_id",
-           "eduperson_entitlement",
+           "eduperson_entitlement"
        ],
-       "request_parameter_supported": True,
-       "request_uri_parameter_supported": True,
-       "require_request_uri_registration": True,
+       "request_parameter_supported": true,
+       "request_uri_parameter_supported": true,
+       "require_request_uri_registration": true,
        "code_challenge_methods_supported": ["plain", "S256"],
-       "tls_client_certificate_bound_access_tokens": True,
+       "tls_client_certificate_bound_access_tokens": true,
        "revocation_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/revoke",
        "revocation_endpoint_auth_methods_supported": [
            "private_key_jwt",
            "client_secret_basic",
            "client_secret_post",
            "tls_client_auth",
-           "client_secret_jwt",
+           "client_secret_jwt"
        ],
        "revocation_endpoint_auth_signing_alg_values_supported": [
            "PS384",
@@ -263,10 +263,10 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "ES512",
            "PS256",
            "PS512",
-           "RS512",
+           "RS512"
        ],
-       "backchannel_logout_supported": True,
-       "backchannel_logout_session_supported": True,
+       "backchannel_logout_supported": true,
+       "backchannel_logout_session_supported": true,
        "device_authorization_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/auth/device",
        "backchannel_token_delivery_modes_supported": ["poll", "ping"],
        "backchannel_authentication_request_signing_alg_values_supported": [
@@ -278,9 +278,9 @@ class CheckinOpenIdConnectTest(OpenIdConnectTestMixin, OAuth2Test):
            "ES512",
            "PS256",
            "PS512",
-           "RS512",
+           "RS512"
        ],
-       "require_pushed_authorization_requests": False,
+       "require_pushed_authorization_requests": false,
        "mtls_endpoint_aliases": {
            "token_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/token",
            "revocation_endpoint": "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/revoke",
