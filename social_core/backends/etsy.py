9d3ba91a632e4bf031842558aedaa5a7cf64ae7d
Amit Ray <51674969+amitray007@users.noreply.github.com>
2024-01-12 22:46:29 +0530

    feat: Added Backend 'EtsyOAuth2' (#874)
    * feat: Added Backend 'EtsyOAuth2'
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ social_core/backends/etsy.py
@@ -0,0 +1,42 @@
+from .oauth import BaseOAuth2PKCE
+
+
+class EtsyOAuth2(BaseOAuth2PKCE):
+    name = "etsy"
+    ID_KEY = "user_id"
+    AUTHORIZATION_URL = "https://www.etsy.com/oauth/connect"
+    ACCESS_TOKEN_URL = "https://api.etsy.com/v3/public/oauth/token"
+    REFRESH_TOKEN_URL = "https://api.etsy.com/v3/public/oauth/token"
+    ACCESS_TOKEN_METHOD = "POST"
+    REQUEST_TOKEN_METHOD = "POST"
+    SCOPE_SEPARATOR = " "
+    EXTRA_DATA = [
+        ("refresh_token", "refresh_token"),
+        ("expires_in", "expires_in"),
+        ("token_type", "token_type"),
+        ("access_token", "access_token"),
+        # User Data Fields
+        ("primary_email", "primary_email"),
+        ("first_name", "first_name"),
+        ("last_name", "last_name"),
+        ("image_url_75x75", "image_url_75x75"),
+    ]
+
+    def user_data(self, access_token, *args, **kwargs) -> dict:
+        client_id, _ = self.get_key_and_secret()
+        user_id = access_token.split(".")[0]
+        headers = {"Authorization": f"Bearer {access_token}", "x-api-key": client_id}
+        return self.get_json(
+            url=f"https://openapi.etsy.com/v3/application/users/{user_id}",
+            headers=headers,
+        )
+
+    def get_user_details(self, response):
+        return {
+            "user_id": response["user_id"],
+            "first_name": response["first_name"],
+            "last_name": response["last_name"],
+            "email": response["primary_email"],
+            "image_url_75x75": response["image_url_75x75"],
+            "username": response["user_id"],
+        }
