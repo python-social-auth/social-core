fd33a9a5c68e3bbd9f938c69478ad2c20380b3d0
Danyal Faheem <138459282+Danyal-Faheem@users.noreply.github.com>
2025-02-03 20:39:24 +0500

    fix: update base URL to allow custom domains
    fixes #809.
    
    The Authority Host can be defined to be a custom domain as well using front door in Azure. With this, the tenant name is not prefixed to the authority host in the base url.
    
    We update this by prefixing the tenant name with the default authority host only if the authority host is not defined.

--- social_core/backends/azuread_b2c.py
+++ social_core/backends/azuread_b2c.py
@@ -41,7 +41,7 @@ from .azuread import AzureADOAuth2
 class AzureADB2COAuth2(AzureADOAuth2):
     name = "azuread-b2c-oauth2"
 
-    BASE_URL = "https://{tenant_name}.{authority_host}/{tenant_name}.onmicrosoft.com"
+    BASE_URL = "https://{authority_host}/{tenant_name}.onmicrosoft.com"
     AUTHORIZATION_URL = "{base_url}/oauth2/v2.0/authorize"
     OPENID_CONFIGURATION_URL = (
         "{base_url}/v2.0/.well-known/openid-configuration?p={policy}"
@@ -64,7 +64,7 @@ class AzureADB2COAuth2(AzureADOAuth2):
 
     @property
     def authority_host(self):
-        return self.setting("AUTHORITY_HOST", "b2clogin.com")
+        return self.setting("AUTHORITY_HOST", f"{tenant_name}.b2clogin.com")
 
     @property
     def tenant_name(self):
