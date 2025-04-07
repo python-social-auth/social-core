893dbace94887a9120988156cabdbd6c1c522094
cdelafuen <christian.delafuente.barrio@gmail.com>
2025-04-07 13:59:23 +0200

    Updated API information for users

--- social_core/backends/kick.py
+++ social_core/backends/kick.py
@@ -7,7 +7,9 @@ from .oauth import BaseOAuth2PKCE
 
 
 class KickOAuth2(BaseOAuth2PKCE):
-    """Kick OAuth2 authentication backend"""
+    """Kick OAuth2 authentication backend
+    API docs at: https://docs.kick.com/apis/users
+    """
     name = 'kick'
     HOSTNAME = 'id.kick.com'
     API_HOSTNAME = 'kick.com'
@@ -29,15 +31,22 @@ class KickOAuth2(BaseOAuth2PKCE):
     def get_user_details(self, response):
         """Return user details from Kick account"""
         return {
-            'username': response.get('username'),
+            'username': response.get('name'),  # API returns 'name' instead of 'username'
             'email': response.get('email') or '',
-            'fullname': response.get('display_name') or '',
+            'fullname': response.get('name') or '',  # Using 'name' as fullname
             'first_name': '',
-            'last_name': ''
+            'last_name': '',
+            'profile_picture': response.get('profile_picture') or '',
+            'user_id': response.get('user_id')
         }
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from service"""
-        url = f'https://api.{self.API_HOSTNAME}/v1/user'
+        url = f'https://api.{self.API_HOSTNAME}/public/v1/users'
         auth_header = {'Authorization': f'Bearer {access_token}'}
-        return self.get_json(url, headers=auth_header)
+        response = self.get_json(url, headers=auth_header)
+        # The API returns data in a 'data' field with an array of users
+        # For the authenticated user (when no user IDs are specified), we get the first item
+        if response and 'data' in response and response['data']:
+            return response['data'][0]
+        return {}
