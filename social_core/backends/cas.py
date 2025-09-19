35b54a16b89ca9a55fd8d407dc3840aecef8a7d5
Michal Čihař <michal@cihar.com>
2025-09-19 09:03:07 +0200

    feat: introduce logging to the backend classes

--- social_core/backends/cas.py
+++ social_core/backends/cas.py
@@ -7,11 +7,7 @@ the minor implementation differences between the Apereo CAS OIDC server
 implementation and the standard OIDC implementation in Python Social Auth.
 """
 
-import logging
-
 from .open_id_connect import OpenIdConnectAuth
-
-logger = logging.getLogger("social")
 
 
 class CASOpenIdConnectAuth(OpenIdConnectAuth):
@@ -33,25 +29,23 @@ class CASOpenIdConnectAuth(OpenIdConnectAuth):
 
     def oidc_endpoint(self):
         endpoint = self.setting("OIDC_ENDPOINT", self.OIDC_ENDPOINT)
-        logger.debug("backend: CAS, endpoint: %s", endpoint)
+        self.log_debug("endpoint: %s", endpoint)
         return endpoint
 
     def get_user_id(self, details, response):
-        logger.debug(
-            "backend: CAS, method: get_user_id, details: %s, %s", details, response
-        )
+        self.log_debug("method: get_user_id, details: %s, %s", details, response)
         return details.get("username")
 
     def user_data(self, access_token, *args, **kwargs):
         data = self.get_json(
             self.userinfo_url(), headers={"Authorization": f"Bearer {access_token}"}
         )
-        logger.debug("backend: CAS, user_data: %s", data)
+        self.log_debug("user_data: %s", data)
         return data.get("attributes", {})
 
     def get_user_details(self, response):
         username_key = self.setting("USERNAME_KEY", self.USERNAME_KEY)
-        logger.debug("backend: CAS, username_key: %s", username_key)
+        self.log_debug("username_key: %s", username_key)
         attributes = self.user_data(response.get("access_token"))
         return {
             "username": attributes.get(username_key),
