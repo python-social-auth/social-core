8e962d380ce8ff5ed00a49938292e67a70d110a2
Michal Čihař <michal@cihar.com>
2025-11-20 10:57:15 +0100

    chore(backend): drop no longer supported OAuth1 backend for Yahoo
    - Drop OAuth1 backend and tests
    - Add OAuth2 tests
    - Fixed OAuth2 username mapping to match documentation
    
    Issue #1176

--- social_core/backends/yahoo.py
+++ social_core/backends/yahoo.py
@@ -7,54 +7,7 @@ from requests.auth import HTTPBasicAuth
 
 from social_core.utils import handle_http_errors
 
-from .oauth import BaseOAuth1, BaseOAuth2
-
-
-class YahooOAuth(BaseOAuth1):
-    """Yahoo OAuth authentication backend. DEPRECATED"""
-
-    name = "yahoo-oauth"
-    ID_KEY = "guid"
-    AUTHORIZATION_URL = "https://api.login.yahoo.com/oauth/v2/request_auth"
-    REQUEST_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_request_token"
-    ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_token"
-    EXTRA_DATA = [
-        ("guid", "id"),
-        ("access_token", "access_token"),
-        ("expires", "expires"),
-    ]
-
-    def get_user_details(self, response):
-        """Return user details from Yahoo Profile"""
-        fullname, first_name, last_name = self.get_user_names(
-            first_name=response.get("givenName"), last_name=response.get("familyName")
-        )
-        emails = [email for email in response.get("emails", []) if email.get("handle")]
-        emails.sort(key=lambda e: e.get("primary", False), reverse=True)
-        return {
-            "username": response.get("nickname"),
-            "email": emails[0]["handle"] if emails else "",
-            "fullname": fullname,
-            "first_name": first_name,
-            "last_name": last_name,
-        }
-
-    def user_data(self, access_token, *args, **kwargs):
-        """Loads user data from service"""
-        url = "https://social.yahooapis.com/v1/user/{0}/profile?format=json"
-        return self.get_json(
-            url.format(self._get_guid(access_token)), auth=self.oauth_auth(access_token)
-        )["profile"]
-
-    def _get_guid(self, access_token):
-        """
-        Because you have to provide GUID for every API request it's also
-        returned during one of OAuth calls
-        """
-        return self.get_json(
-            "https://social.yahooapis.com/v1/me/guid?format=json",
-            auth=self.oauth_auth(access_token),
-        )["guid"]["value"]
+from .oauth import BaseOAuth2
 
 
 class YahooOAuth2(BaseOAuth2):
@@ -89,7 +42,7 @@ class YahooOAuth2(BaseOAuth2):
 
         email = response.get("email")
         return {
-            "username": response.get("nickname"),
+            "username": response.get("preferred_username", response.get("nickname")),
             "email": email,
             "fullname": fullname,
             "first_name": first_name,
--- social_core/tests/backends/test_yahoo.py
+++ social_core/tests/backends/test_yahoo.py
@@ -1,85 +1,36 @@
-# pyright: reportAttributeAccessIssue=false
 import json
-from urllib.parse import urlencode
 
-import requests
-import responses
-
-from .oauth import OAuth1AuthUrlTestMixin, OAuth1Test
+from .oauth import BaseAuthUrlTestMixin, OAuth2Test
 
 
-class YahooOAuth1Test(OAuth1Test, OAuth1AuthUrlTestMixin):
-    backend_path = "social_core.backends.yahoo.YahooOAuth"
-    user_data_url = "https://social.yahooapis.com/v1/user/a-guid/profile?format=json"
-    expected_username = "foobar"
-    access_token_body = json.dumps({"access_token": "foobar", "token_type": "bearer"})
-    request_token_body = urlencode(
+class YahooOAuth2Test(OAuth2Test, BaseAuthUrlTestMixin):
+    backend_path = "social_core.backends.yahoo.YahooOAuth2"
+    user_data_url = "https://api.login.yahoo.com/openid/v1/userinfo"
+    expected_username = "j.doe"
+    access_token_body = json.dumps(
         {
-            "oauth_token_secret": "foobar-secret",
-            "oauth_token": "foobar",
-            "oauth_callback_confirmed": "true",
+            "access_token": "UNQO1djO5xpaKm3_KbECBKB5mlFr6tSZTOLrrJCprtT1X1UFljpxiS5iSue8u_n8ah1WbL6sTNw3HPFHicyXDbTs7aSrbIe.rx9n9dzX7xZjx8dyF2Ap1a6J_nw4k56a5mCOuTd.ZFQENgGtHwM0DRFVeDNTAx_WzhqDGPCqhtsNICuuY30soFZGS11FTlUk7Gy0ISjxLRAjIZVtpojnY5p8XuT1qUtAheWqZegJ_7t.AP4o0J4xJ3_oocXeiSKEXaD3AijdBdViKPZI3Ow7yeHK8uX1weNfKoSP6eEpCviyj0YlRMIBSg4cRdGL6EsSggX6B5gzgcA9efDSpcwVhupY0RlUdi.AxJ1nT0frWmrYiwntpu1XP_5mIbOlb4wfrD_ZCRNY2Qby40RBt5iHERSJ89K1o69fw3Jd4C3hF14iJLHcDHmnYJSX651G9MlpGPWT99DRteCdhSm8URbZqfGPG8mZtLpmhfxr1umCoGEgocrfHpITMjOyEwvgmAhgjGKXugvdNTABn0AEQBetIVtJ80Ymbn6IMq_Qh10vyspVsVK69C9yTlwLtZhcvim5Nk_15JHd0GSj0Mj.X.FWTzUK1e3CNQjeJxdQ2Qk9BXDC4_DXW_Ot5LzYy5qRvRKT4gh54n5aBROxFdky0ELt1IgkLTRJ0idUCen87klP.0CLp1QTNXx99N6nM9c_HwWVKwhILUjzXaIrP0GVEMwlGIHqn2I91Z03irBgzrMB219lqUAuF27_OD4QnyQfICSW65n5hVo1e89xwN6VN3usRrhHmdDfd7nk3nzMyXdsOPzghA1huBCYyEGZ_kq9FzVFQ5QYDmJ0WqpmG1yXDEntYVvkB_i_jkbNPH4.R134ptwznCZSuQ--",
+            "refresh_token": "AJj.Dlbt_e4XN85buQhFXj77sIB3lqBF3Bcqb2kwUEoYrBb0Pg--",
+            "expires_in": 3600,
+            "token_type": "bearer",
+            "xoauth_yahoo_guid": "UQIDWJNWVNQD4GXZ5NGMZUSTQ4",
+            "id_token": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjM0NjZkNTFmN2RkMGM3ODA1NjU2ODhjMTgzOTIxODE2YzQ1ODg5YWQifQ.eyJhdF9oYXNoIjoiYWM5YkR3ejVMWjl5UEVpdWtEcGdzdz09Iiwic3ViIjoiVVFJRFdKTldWTlFENEdYWjVOR01aVVNUUTQiLCJhdWQiOiJkajB5Sm1rOVdHeDBRbEUwVVdkQ2EwaEtKbVE5V1Zkck9XTnJOVWhYVm5CaFRraEZiV05IYnpsTlFTMHRKbk05WTI5dWMzVnRaWEp6WldOeVpYUW1lRDAxT0EtLSIsImlzcyI6Imh0dHBzOi8vbG9naW4ueWFob28uY29tIiwiZXhwIjoxNDQzODI3MTMwLCJub25jZSI6IjEyMzQ1IiwiaWF0IjoxNDQzODIzNTMwfQ.n7oEFi5028StcI41Hkh6lLYK4PmF7pT4AIXrQ_62nfDEZj2g0oYjSLFPJp4IqF6LefwcCQ9FHT5X9eC8A7peqw",
         }
-    )
-    guid_body = json.dumps(
-        {"guid": {"uri": "https://social.yahooapis.com/v1/me/guid", "value": "a-guid"}}
     )
     user_data_body = json.dumps(
         {
-            "profile": {
-                "bdRestricted": True,
-                "memberSince": "2007-12-11T14:40:30Z",
-                "image": {
-                    "width": 192,
-                    "imageUrl": "http://l.yimg.com/dh/ap/social/profile/"
-                    "profile_b192.png",
-                    "size": "192x192",
-                    "height": 192,
-                },
-                "created": "2013-03-18T04:15:08Z",
-                "uri": "https://social.yahooapis.com/v1/user/a-guid/profile",
-                "isConnected": False,
-                "profileUrl": "http://profile.yahoo.com/a-guid",
-                "guid": "a-guid",
-                "nickname": "foobar",
-                "emails": [
-                    {
-                        "handle": "foobar@yahoo.com",
-                        "id": 1,
-                        "primary": True,
-                        "type": "HOME",
-                    },
-                    {
-                        "handle": "foobar@email.com",
-                        "id": 2,
-                        "type": "HOME",
-                    },
-                ],
-            }
+            "sub": "FSVIDUW3D7FSVIDUW3D72F2F",  # user identifier
+            "name": "Jane Doe",
+            "given_name": "Jane",
+            "family_name": "Doe",
+            "preferred_username": "j.doe",
+            "email": "janedoe@example.com",
+            "picture": "http://example.com/janedoe/me.jpg",
         }
     )
 
     def test_login(self) -> None:
-        responses.add(
-            responses.GET,
-            "https://social.yahooapis.com/v1/me/guid?format=json",
-            status=200,
-            body=self.guid_body,
-        )
         self.do_login()
 
     def test_partial_pipeline(self) -> None:
-        responses.add(
-            responses.GET,
-            "https://social.yahooapis.com/v1/me/guid?format=json",
-            status=200,
-            body=self.guid_body,
-        )
         self.do_partial_pipeline()
-
-    def test_get_user_details(self) -> None:
-        responses.add(
-            responses.GET, self.user_data_url, status=200, body=self.user_data_body
-        )
-        response = requests.get(self.user_data_url, timeout=1)
-        user_details = self.backend.get_user_details(response.json()["profile"])
-        self.assertEqual(user_details["email"], "foobar@yahoo.com")
